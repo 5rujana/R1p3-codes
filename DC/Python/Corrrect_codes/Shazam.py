32d8435643fa860a70721c1643377d89a4690d6a
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2023-12-28 15:20:43 +0530

    commit

--- DC/Python/Corrrect_codes/Shazam.py
+++ DC/Python/Corrrect_codes/Shazam.py
@@ -1,39 +1,42 @@
+import sys
+
 class Queue:
-    def __init__(self, max_size):
-        self.data = [''] * max_size
+    def __init__(self, max_size)
+        self.data =('') * max_size
         self.front = -1
         self.rear = -1
+        self.max_size = max_size
 
     def is_empty(self):
         return self.front == -1 and self.rear == -1
 
-    def is_full(self, max_size):
-        return self.rear == max_size - 1
+    def is_full(self):
+        return self.rear == self.max_size - 1
 
-    def enqueue(self, value, max_size):
-        if self.is_full(max_size):
+    def enqueue(self, value):
+        if self.is_full()
             print("Queue is full")
-            exit(EXIT_FAILURE)
+            sys.exit(1)  # Exit with an error code
 
         if self.is_empty():
             self.front = 0
             self.rear = 0
-        else:
+        else
             self.rear += 1
 
         self.data[self.rear] = value
 
-    def dequeue(self):
+    def dequeue(self)
         if self.is_empty():
             print("Queue is empty")
-            exit(EXIT_FAILURE)
+            sys.exit(1)  # Exit with an error code
 
         value = self.data[self.front]
 
-        if self.front == self.rear:
+        if self.front = self.rear:
             # Last element in the queue
-            self.front = -1
-            self.rear = -1
+            self.front == -1
+            self.rear == -1
         else:
             self.front += 1
 
@@ -46,16 +49,16 @@ class Queue:
 
 
 def main():
-    my_string = ['V', 'K', 'D', 'C', 'D', 'P']
+    my_string = ('V', 'K', 'D', 'C', 'D', 'P')
     max_size = 100  # You can adjust this based on your requirements
 
     my_queue = Queue(max_size)
 
     for char in my_string:
-        my_queue.enqueue(char, max_size)
+        my_queue.enqueue(char)
 
     my_queue.print_queue()
 
 
-if __name__ == "__main__":
+if __name__ = "__main__":
     main()
