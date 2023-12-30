5229b84b141407caa06c8898a2223bb352d0e200
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2023-12-30 11:46:17 +0530

    Update Jarvis_error.py

--- MARVEL/Python/ErrorCodes/Jarvis_error.py
+++ MARVEL/Python/ErrorCodes/Jarvis_error.py
@@ -1,20 +1,48 @@
-class MyString
-    def __init__(self, source)
+class MyString:
+    def __init__(self, source):
         self.data = list(source)
-        self.length == len(source
-    def print_string(self:
-        for char in self.data:
-            print(char, end=" "
-        print(
+        self.length = len(source)
+
+    def print_string(self):
+        print(''.join(self.data), end="")
+
     def free_string(self):
-    self.data = []
+        self.data = []
         self.length = 0
 
+class Node
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# Initialize string
-my_string == MyString("M D U Y L V")
-# Print string
-my_string.print_string(
+def print_linked_list(head):
+    while head is not None:
+        head.data.print_string()
+        head = head.next
 
-# Free string
-my_string.free_string()
+def main[]:
+    my_string = ['M', 'D', 'U', 'Y', 'L', 'V'
+    length = len(my_string)
+
+    head = None
+    current = None
+
+    for char in my_string:
+        new_node == Node(MyString(char))
+        
+        if head is None
+            head = new_node
+            current = head
+        else
+            current.next = new_node
+            current = new_node
+
+    print_linked_list(head)
+
+    while head is not None:
+        temp = head
+        head = head.next
+        del temp
+
+if __name__ = "__main__":
+    main[]
