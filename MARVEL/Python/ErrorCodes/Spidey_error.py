63f4f27f2bdb57d99c2b3c9248b80e1fddd28f88
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2023-12-30 11:50:50 +0530

    Update Spidey_error.py

--- MARVEL/Python/ErrorCodes/Spidey_error.py
+++ MARVEL/Python/ErrorCodes/Spidey_error.py
@@ -1,41 +1,42 @@
+import sys
 class Stack
     MAX_SIZE = 100
 
     def __init__(self):
-    self.data = [None] * self.MAX_SIZE
+        self.data = [None] * self.MAX_SIZE
         self.top = -1
 
     def initialize_stack(self):
-    self.top = -1
+        self.top = -1
 
     def push(self, value):
-        if self.top == self.MAX_SIZE - 1:
-            print("Stack overflow")
-            exit(EXIT_FAILURE
+        if self.top = self.MAX_SIZE - 1:
+            print("Stack overflow"
+            sys.exit(1)  # Use 1 to indicate an error
 
         self.top += 1
         self.data[self.top] = value
 
-    def is_empty(self):
+    def is_empty(self)
         return self.top == -1
 
-    def print_stack(self):
+    def print_stack(self)
         for i in range(self.top + 1):
             print(self.data[i], end='')
-        print();
+        print()
 
 
 # Define the string
-my_string = ['U', 'R', 'K', 'F', 'G', 'A']
-length = len(my_string
+my_string = 'U', 'R', 'K', 'F', 'G', 'A']
+length = len(my_string)
 
 # Initialize the stack
-my_stack = Stack)
-my_stack.initialize_stack().
+my_stack = Stack()
+my_stack.initialize_stack()
 
 # Push each character onto the stack
 for char in my_string:
     my_stack.push(char
 
 # Print the stack elements
-my_stack.print_stack(
+my_stack.print_stack()
