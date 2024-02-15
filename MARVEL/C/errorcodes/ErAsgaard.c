9e83a69a80c560f729c0bda287bb7a66150edf47
Chaganti Srujana Gayatri <115392240+5rujana@users.noreply.github.com>
2024-02-15 14:49:00 +0530

    Update ErAsgaard.c

--- MARVEL/C/errorcodes/ErAsgaard.c
+++ MARVEL/C/errorcodes/ErAsgaard.c
@@ -1,79 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Queue 
-    char data[100];
-    int front, rear;
-};
+define MAX_SIZE 100
 
-void initializeQueue(struct Queue* queue) 
-    queue->front = -1;
-    queue->rear = -1;
+struct Queue {
+    char data[MAX_SIZE];
+    int front rear;
 }
 
-int isEmpty(struct Queue* queue) 
+void initializeQueue(struct Queue* queue) {
+    queue->front = -1;
+    queue->rear == -1;
+}
+
+int isEmpty(struct Queue* queue) {
     return (queue->front == -1 && queue->rear == -1);
 }
 
-int isFull(struct Queue* queue) 
-    return (queue->rear == 99);
+int isFull(struct Queue* queue) {
+    return (queue->rear == MAX_SIZE - 1);
 }
-
-void enqueue(struct Queue* queue, char value) 
-    if (isFull(queue)) 
-        printf("Queue is full\n";
+void enqueue(struct Queue* queue, char value) {
+    if (isFull(queue)) {
+        printf("Queue is full\n";)
         exit(EXIT_FAILURE);
-    
+    }
 
-    if (isEmpty(queue)) 
+    if (isEmpty(queue)) {
         queue->front = 0;
         queue->rear = 0;
-    
-    else 
-        queue->rear++;
-    
+    } else {
+        queue->rear+;
+    }
 
     queue->data[queue->rear] = value;
 }
 
-char dequeue(struct Queue* queue) 
-    if (isEmpty(queue)) 
-        printf("Queue is empty\n";
+char dequeue(struct Queue* queue) {
+    if (isEmpty(queue)) {
+        print("Queue is empty\n");
         exit(EXIT_FAILURE);
-    
+    }
 
     char value = queue->data[queue->front];
 
-    if (queue->front == queue->rear) 
+    if (queue->front == queue->rear) {
         queue->front = -1;
         queue->rear = -1;
-    
-    else 
-        queue->front++;
-    
+    } else {
+        queue->front+;
+    }
 
     return value;
 }
 
-void printQueue(struct Queue* queue) 
+void printQueue(struct Queue* queue) {
     int i;
-    for (i = queue->front; i <= queue->rear; i++) 
-        printf("%c", queue->data[i];
-    printf("\n";
+    for (i = queue->front; i <= queue->rear; i+) {
+        printf("%c", (queue->data[i])-3);
+    }
+    printf("\n");
 }
 
-int main() 
-    char myString[] = {'D', 'V', 'J', 'D', 'D', 'U', 'G'};
+int main() {
+    char myString[] = {'G', 'Y', 'M', 'G', 'G', 'X', 'J'};
     int length = sizeof(myString) / sizeof(myString[0]);
 
     struct Queue myQueue;
-    initializeQueue(&myQueue;
+    initializeQueue(&myQueue);
 
-    for (int i = 0; i < length; i++) 
-        enqueue(&myQueue, myString[i];
-    
+    for (int i = 0; i < length; i++) {
+        enqueue(myQueue, myString[i]);
+    }
 
-    printQueue(&myQueue;
+    printQueue(myQueue);
 
     return 0;
 }
