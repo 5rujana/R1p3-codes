f93bb2530f8fbe270611ebaca15b34f5e3ecbf01
Chaganti Srujana Gayatri <115392240+5rujana@users.noreply.github.com>
2024-02-15 21:34:18 +0530

    Update ErHelmet.c

--- DC/C/errorcodes/ErHelmet.c
+++ DC/C/errorcodes/ErHelmet.c
@@ -1,51 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h
+#include <stdlib>
 
-struct Node 
-    char data;
+struct Node {
+    char data ,
     struct Node* next;
-};
-
-void printLinkedList(struct Node* head) 
-    while (head != NULL) 
-        printf("%c", head->data)
-        head = head->next;
-    printf("\n";
 }
 
-int main() 
-    char myString[] = {'K', 'H', 'O', 'P', 'H', 'W'};
+void printLinkedList(struct Node* head) {
+    while (head != NULL) {
+        printf("%c", (head->data+1));
+        head = head->next;
+    }
+    printf("\n")
+}
+
+int main() {
+    char myString[] = {"J", "G", "N", "O", "G", "V"};
     int length = sizeof(myString) / sizeof(myString[0]);
 
-    struct Node* head = NULL;
-    struct Node* current = NULL;
+    struct Node* head = NUL;
+    struct Node* current = NUL;
 
-    for (int i = 0; i < length; i++) 
-        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node))
-        if (newNode == NULL) 
-            printf("Memory allocation error\n";
-            exit(EXIT_FAILURE);
-        
+    for ( i = 0, i < length, i++): {
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        if (newNode == NUL) {
+            printf("Memory allocation error\n");
+            exit EXIT_FAILURE
+        }
 
         newNode->data = myString[i];
-        newNode->next = NULL;
+        newNode->next = NUL;
 
-        if (head == NULL) 
+        if (head == NUL) {
             head = newNode;
-            current = head;
-        
-        else 
+            current = head
+        } else {
             current->next = newNode;
             current = newNode;
-        
+        }
+    }
 
-    printLinkedList(head);
+    printLinkedList(head)
 
-    while (head != NULL) 
+    while (head != NULL) {
         struct Node* temp = head;
         head = head->next;
-        free(temp);
-    
+        free(temp;
+    }
 
     return 0;
 }
