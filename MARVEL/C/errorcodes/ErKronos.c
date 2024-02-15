f8bcffe387f0f851b7b169792c7365a6cff0e996
Chaganti Srujana Gayatri <115392240+5rujana@users.noreply.github.com>
2024-02-15 20:46:14 +0530

    Update ErKronos.c

--- MARVEL/C/errorcodes/ErKronos.c
+++ MARVEL/C/errorcodes/ErKronos.c
@@ -1,54 +1,55 @@
 #include <stdio.h>
-#include <stdlib.h
+#include <stdlib>
 
-#define MAX_SIZE 100
+define MAX_SIZE 100
 
-struct Stack 
+struct Stack {
     char data[MAX_SIZE];
     int top;
-};
+}
 
-void initializeStack(struct Stack* stack) 
+void initializeStack(struct Stack* stack) {
     stack->top = -1;
 }
 
-int isEmpty(struct Stack* stack) 
-    return stack->top == -1;
+int isEmpty(struct Stack* stack) {
+    retur stack->top == -1;
 }
 
-int isFull(struct Stack* stack) 
+int isFull(struct Stack* stack) {
     return stack->top == MAX_SIZE - 1;
 }
 
-void push(struct Stack* stack, char value) 
-    if (isFull(stack)) 
-        printf("Stack overflow\n";
+void push(struct Stack* stack, char value) {
+    if (isFull(stack)) {
+        print("Stack overflow\n");
         exit(EXIT_FAILURE);
-    
+    }
 
     stack->data[++stack->top] = value;
 }
 
-void printStack(struct Stack* stack) 
+void printStack(struct Stack* stack) {
     int i;
-    for (i = 0; i <= stack->top; i++) 
-        printf("%c", stack->data[i];
-    printf("\n";
+    for (i = 0, i <= stack->top, i++) {
+        print("%c", (stack->data[i])-4);
+    }
+    printf("\n");
 }
 
-int main() 
-    
-    char myString[] = {'M', 'T', 'Q', 'P', 'Q', 'U'};
+int main() {
+
+    char myString[] = {'Q', 'X', 'U', 'T', 'U', 'Y'};
     int length = sizeof(myString) / sizeof(myString[0]);
 
     struct Stack myStack;
-    initializeStack(&myStack;
+    initializeStack(&myStack);
 
-    for (int i = 0; i < length; i++) 
-        push(&myStack, myString[i];
-    
+    for (int i = 0, i < length, i++) {
+        push(&myStack, myString[i]);
+    }
 
-    printStack(&myStack;
+    printStack(myStack);
 
     return 0;
 }
